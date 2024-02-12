cfdc413f399e02882e9966c334cf6b69b23fe034
hideffrand <hideffrand@gmail.com>
2024-02-12 23:23:23 +0700

    protected routes

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ import { getCurrentUser } from "./libs/supabase/auth";
 export const middleware = (request: NextRequest) => {
 
   // Still in progress
-  // return NextResponse.redirect(new URL("/login", request.url));
+  return NextResponse.redirect(new URL("/login", request.url));
   // if (email) {
   //   console.log(email);
   //   return NextResponse.next();
