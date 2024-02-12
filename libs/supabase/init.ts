871c77022031bb2e0961ac8ebb4a74617500ea55
hideffrand <hideffrand@gmail.com>
2024-02-12 20:08:40 +0700

    fix supabase init issue #3

--- libs/supabase/init.ts
+++ libs/supabase/init.ts
@@ -1,9 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseProjectUrl = process.env.NEXT_PUBLIC_SUPABASE_PROJECT_URL || '';
+// const supabaseProjectUrl = process.env.NEXT_PUBLIC_SUPABASE_PROJECT_URL || '';
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 
 export const supabase = createClient(
-  supabaseProjectUrl,
+  "https://fqxdoarlbriiufofnlqj.supabase.co",
   supabaseAnonKey
 );
